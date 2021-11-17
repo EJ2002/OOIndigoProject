d1207b8633bb408738c899478800f75ccab5695f
EJ2002 <74745004+EJ2002@users.noreply.github.com>
2021-11-17 13:24:34 +0800

    Update VSAPCalculator.py

--- .py scripts/VSAPCalculator.py
+++ .py scripts/VSAPCalculator.py
@@ -1,4 +1,4 @@
-from Formulas import Spheres, Cylinder
+from Formulas import Spheres, Cylinder, Cone, SquarePyramid, Rectangulartank, Cube
 
 def menu():
 
@@ -23,12 +23,14 @@ try:
         command = int(input("Enter command: "))
 
         if command == 1:
+
             r = int(input("Enter Radius of Sphere: "))
             s = Spheres(r)
             print("The Surface Area of the Sphere is: ", round(s.SA(), 2))
             print("The Volume of the Sphere is: ", round(s.V(), 2))
 
         elif command == 2:
+
             r = int(input('Enter Radius of Cylinder: '))
             h = int(input("Enter Height of Cylinder: "))
             c = Cylinder(r, h)
@@ -36,24 +38,50 @@ try:
             print("The Volume of the Cylinder is: ", round(c.V(), 2))
 
         elif command == 3:
+
             r = int(input('Enter Base Radius of the Cone: '))
             h = int(input('Enter the Height of the Cone: '))
+            c = Cone(r, h)
+            print("The Surface area of the Cone is: ", round(c.SA(), 2))
+            print("The Volume of the Cone is: ", round(c.V(), 2))
 
         elif command == 4:
-            b = int(input('Enter Base Edge of the Square Pyramid'))
+
+            a = int(input('Enter Base Edge of the Square Pyramid: '))
+            h = int(input('Enter the Height of the Square Pyramid: '))
+            Sp = SquarePyramid(a, h)
+            print("The Surface area of the Square Pyramid is: ", round(Sp.SA(), 2))
+            print("The Volume of the Square Pyramid is: ", round(Sp.V(), 2))
+
         elif command == 5:
-            l = int(input('Enter length of the Rectangular Tank'))
-            w = int(input('Enter width of the Rectangular Tank'))
-            h = int(input('Enter Height of the Rectangular Tank'))
+
+            l = int(input('Enter length of the Rectangular Tank: '))
+            w = int(input('Enter width of the Rectangular Tank: '))
+            h = int(input('Enter Height of the Rectangular Tank: '))
+            Rt = Rectangulartank(w, h, l)
+            print("The Surface area of the Rectangular Tank is: ", round(Rt.SA(), 2))
+            print("The Volume of the Rectangular Tank is: ", round(Rt.V(), 2))
+
         elif command == 6:
-            el = int(input('Enter Edge Length of Cube: '))
-            break
+
+            a = int(input('Enter Edge Length of Cube: '))
+            c = Cube(a)
+            print("The Surface area of the Cube is: ", round(c.SA(), 2))
+            print("The Volume of the Cube is: ", round(c.V(), 2))
+
         elif command == 7:
+
             menu()
+
         elif command == 8:
+
             print("Thank you for using the program.")
             break
+
         else:
+
             print("Please enter a valid command.")
+
 except ValueError:
+
     print("You have entered a non-numeral value.")
